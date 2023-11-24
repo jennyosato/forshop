a1d692c92ed00ab5c193bc5680ef02173f30274c
Gbenga Adeyi <adeyigbenga005@gmail.com>
2023-11-24 13:29:39 +0100

    Convert to ts file

--- src/pages/api/auth/[...nextauth].ts
+++ src/pages/api/auth/[...nextauth].ts
@@ -1,18 +1,17 @@
-import NextAuth from "next-auth"
+import NextAuth from "next-auth";
 // import GithubProvider from "next-auth/providers/github"
 // import FacebookProvider from "next-auth/providers/facebook";
 import GoogleProvider from "next-auth/providers/google";
 
 export const authOptions = {
-  
   // Configure one or more authentication providers
   providers: [
-      GoogleProvider({
-        clientId: process.env.GOOGLE_CLIENT_ID,
-        clientSecret: process.env.GOOGLE_CLIENT_SECRET
-      })
+    GoogleProvider({
+      clientId: process.env.GOOGLE_CLIENT_ID,
+      clientSecret: process.env.GOOGLE_CLIENT_SECRET,
+    }),
     // ...add more providers here
   ],
-}
+};
 
-export default NextAuth(authOptions)
+export default NextAuth(authOptions);
