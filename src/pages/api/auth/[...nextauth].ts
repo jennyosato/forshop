8b60b32453690971e3ce8e630bc015003bc23817
Jennifer <st.jennyandy@gmail.com>
2023-12-05 07:10:13 +0100

    refactor

--- src/pages/api/auth/[...nextauth].ts
+++ src/pages/api/auth/[...nextauth].ts
@@ -2,21 +2,24 @@ import NextAuth from "next-auth";
 // import GithubProvider from "next-auth/providers/github"
 // import FacebookProvider from "next-auth/providers/facebook";
 import GoogleProvider from "next-auth/providers/google";
-
-if (process.env.GOOGLE_CLIENT_ID === undefined) {
-  throw new Error("GOOGLE_CLIENT_ID not set in environmental variables");
-}
-
-if (process.env.GOOGLE_CLIENT_SECRET === undefined) {
-  throw new Error("GOOGLE_CLIENT_SECRET not set in environmental variables");
+const getGoogleCredentials = () => {
+  const clientId = process.env.GOOGLE_CLIENT_ID
+  const clientSecret = process.env.GOOGLE_CLIENT_SECRET
+  if(!clientId || clientId.length < 1){
+    throw new Error("GOOGLE_CLIENT_ID not set in environmental variables");
+  }
+  if(!clientSecret || clientSecret.length < 1){
+    throw new Error("GOOGLE_CLIENT_SECRET not set in environmental variables");
+  }
+  return {clientId, clientSecret}
 }
 
 export const authOptions = {
   // Configure one or more authentication providers
   providers: [
     GoogleProvider({
-      clientId: process.env.GOOGLE_CLIENT_ID,
-      clientSecret: process.env.GOOGLE_CLIENT_SECRET,
+      clientId: getGoogleCredentials().clientId,
+      clientSecret: getGoogleCredentials().clientSecret,
     }),
     // ...add more providers here
   ],
